3e2dc5b7598d45ad3a5f23faf714b9516451e8b8
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-07-26 16:30:24 -0500

    new commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+
+const app = express()
+
+const path = require('path')
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname,'server/index.html'))
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port,() => {
+    console.log(`Listening on port ${port}`)
+})
+
